5f671b958b7a924883f1d2d4bce01a169ebaf1ef
Matthew Congrove <mattcongrove@gmail.com>
2013-08-13 21:27:51 -0400

    Starting work on menu

--- src/countdown.c
+++ src/countdown.c
@@ -10,13 +10,20 @@
 #include "PDutils.h"
 #include "config.h"
 
+#define NUM_MENU_SECTIONS 1
+#define NUM_MENU_ITEMS 5
+
 PBL_APP_INFO(APP_UUID, "Countdown", "Matthew Congrove", 1, 0, RESOURCE_ID_IMAGE_MENU_ICON, APP_TYPE);
 
-Window window;
-TextLayer timeLayer;
-TextLayer countdownLayer;
-TextLayer labelLayerOne;
-TextLayer labelLayerTwo;
+Window window_root;
+Window window_menu;
+TextLayer label_time;
+TextLayer label_countdown;
+TextLayer label_row_one;
+TextLayer label_row_two;
+SimpleMenuLayer menu_layer;
+SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
+SimpleMenuItem menu_items[NUM_MENU_ITEMS];
 
 char timeText[] = "00:00";
 char countText[] = "00";
@@ -87,7 +94,7 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
 	
 	itoa2(difference, &countText[0]);
 	
-	text_layer_set_text(&countdownLayer, countText);
+	text_layer_set_text(&label_countdown, countText);
 	
 	if(clock_is_24h_style()) {
 		time_format = "%R";
@@ -96,52 +103,135 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
 	}
 	
 	string_format_time(timeText, sizeof(timeText), time_format, &now);
-	text_layer_set_text(&timeLayer, timeText);
+	text_layer_set_text(&label_time, timeText);
+}
+
+void menu_select_callback(int index, void *ctx) {
+	switch(index) {
+		case 0:
+			menu_items[index].subtitle = "Hi";
+			break;
+	}
+	
+	layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+}
+
+void menu_button_back(ClickRecognizerRef recognizer, Window *win) {
+	window_stack_pop(true);
+}
+
+void menu_window_click_provider(ClickConfig **config, Window *win) {
+	config[BUTTON_ID_BACK]->click.handler = (ClickHandler) menu_button_back;
+	config[BUTTON_ID_BACK]->long_click.handler = (ClickHandler) menu_button_back;
+}
+
+void menu_open() {
+	window_init(&window_menu, "Menu");
+	window_set_fullscreen(&window_menu, true);
+	window_set_background_color(&window_menu, GColorBlack);
+	window_stack_push(&window_menu, true);
+	window_set_click_config_provider(&window_menu, (ClickConfigProvider) menu_window_click_provider);
+	
+	int counter = 0;
+	
+	menu_items[counter++] = (SimpleMenuItem){
+		.title = "Month",
+		.subtitle = "Hi",
+		.callback = NULL
+	};
+	
+	menu_items[counter++] = (SimpleMenuItem){
+		.title = "Day",
+		.subtitle = "Hi",
+		.callback = NULL
+	};
+	
+	menu_items[counter++] = (SimpleMenuItem){
+		.title = "Year",
+		.subtitle = "Hi",
+		.callback = NULL
+	};
+	
+	menu_items[counter++] = (SimpleMenuItem){
+		.title = "Hour",
+		.subtitle = "Hi",
+		.callback = NULL
+	};
+	
+	menu_items[counter++] = (SimpleMenuItem){
+		.title = "Minute",
+		.subtitle = "Hi",
+		.callback = NULL
+	};
+	
+	menu_sections[0] = (SimpleMenuSection){
+		.num_items = NUM_MENU_ITEMS,
+		.items = menu_items,
+	};
+	
+	simple_menu_layer_init(&menu_layer, window_menu.layer.frame, &window_menu, menu_sections, NUM_MENU_SECTIONS, NULL);
+	
+	layer_add_child(&window_menu.layer, simple_menu_layer_get_layer(&menu_layer));
+}
+
+void root_button_select(ClickRecognizerRef recognizer, Window *win) {
+	menu_open();
+}
+
+void root_window_click_provider(ClickConfig **config, Window *win) {
+	config[BUTTON_ID_SELECT]->click.handler = (ClickHandler) root_button_select;
+	config[BUTTON_ID_SELECT]->long_click.handler = (ClickHandler) root_button_select;
+}
+
+void window_load(Window *window) {
+	text_layer_init(&label_time, window_root.layer.frame);
+	text_layer_set_text_color(&label_time, GColorWhite);
+	text_layer_set_background_color(&label_time, GColorClear);
+	text_layer_set_text_alignment(&label_time, GTextAlignmentCenter);
+	layer_set_frame(&label_time.layer, GRect(0, 13, 144, 30));
+	text_layer_set_font(&label_time, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+	layer_add_child(&window_root.layer, &label_time.layer);
+	
+	text_layer_init(&label_countdown, window_root.layer.frame);
+	text_layer_set_text_color(&label_countdown, GColorWhite);
+	text_layer_set_background_color(&label_countdown, GColorClear);
+	text_layer_set_text_alignment(&label_countdown, GTextAlignmentCenter);
+	layer_set_frame(&label_countdown.layer, GRect(0, 56, 144, 55));
+	text_layer_set_font(&label_countdown, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+	layer_add_child(&window_root.layer, &label_countdown.layer);
+	
+	text_layer_init(&label_row_one, window_root.layer.frame);
+	text_layer_set_text_color(&label_row_one, GColorWhite);
+	text_layer_set_background_color(&label_row_one, GColorClear);
+	text_layer_set_text_alignment(&label_row_one, GTextAlignmentCenter);
+	layer_set_frame(&label_row_one.layer, GRect(0, 110, 144, 23));
+	text_layer_set_text(&label_row_one, LABEL_ROW_1);
+	text_layer_set_font(&label_row_one, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+	layer_add_child(&window_root.layer, &label_row_one.layer);
+	
+	text_layer_init(&label_row_two, window_root.layer.frame);
+	text_layer_set_text_color(&label_row_two, GColorWhite);
+	text_layer_set_background_color(&label_row_two, GColorClear);
+	text_layer_set_text_alignment(&label_row_two, GTextAlignmentCenter);
+	layer_set_frame(&label_row_two.layer, GRect(0, 130, 144, 23));
+	text_layer_set_text(&label_row_two, LABEL_ROW_2);
+	text_layer_set_font(&label_row_two, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+	layer_add_child(&window_root.layer, &label_row_two.layer);
 }
 
 void handle_init(AppContextRef ctx) {
-	window_init(&window, "Countdown");
-	window_set_fullscreen(&window, true);
-	window_set_background_color(&window, GColorBlack);
-	window_stack_push(&window, true);
-	
 	resource_init_current_app(&APP_RESOURCES);
 	
-	text_layer_init(&timeLayer, window.layer.frame);
-	text_layer_set_text_color(&timeLayer, GColorWhite);
-	text_layer_set_background_color(&timeLayer, GColorClear);
-	text_layer_set_text_alignment(&timeLayer, GTextAlignmentCenter);
-	layer_set_frame(&timeLayer.layer, GRect(0, 13, 144, 30));
-	text_layer_set_font(&timeLayer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
-	layer_add_child(&window.layer, &timeLayer.layer);
-	
-	text_layer_init(&countdownLayer, window.layer.frame);
-	text_layer_set_text_color(&countdownLayer, GColorWhite);
-	text_layer_set_background_color(&countdownLayer, GColorClear);
-	text_layer_set_text_alignment(&countdownLayer, GTextAlignmentCenter);
-	layer_set_frame(&countdownLayer.layer, GRect(0, 56, 144, 55));
-	text_layer_set_font(&countdownLayer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
-	layer_add_child(&window.layer, &countdownLayer.layer);
-	
-	text_layer_init(&labelLayerOne, window.layer.frame);
-	text_layer_set_text_color(&labelLayerOne, GColorWhite);
-	text_layer_set_background_color(&labelLayerOne, GColorClear);
-	text_layer_set_text_alignment(&labelLayerOne, GTextAlignmentCenter);
-	layer_set_frame(&labelLayerOne.layer, GRect(0, 110, 144, 23));
-	text_layer_set_text(&labelLayerOne, LABEL_ROW_1);
-	text_layer_set_font(&labelLayerOne, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-	layer_add_child(&window.layer, &labelLayerOne.layer);
-	
-	text_layer_init(&labelLayerTwo, window.layer.frame);
-	text_layer_set_text_color(&labelLayerTwo, GColorWhite);
-	text_layer_set_background_color(&labelLayerTwo, GColorClear);
-	text_layer_set_text_alignment(&labelLayerTwo, GTextAlignmentCenter);
-	layer_set_frame(&labelLayerTwo.layer, GRect(0, 130, 144, 23));
-	text_layer_set_text(&labelLayerTwo, LABEL_ROW_2);
-	text_layer_set_font(&labelLayerTwo, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-	layer_add_child(&window.layer, &labelLayerTwo.layer);
-	
 	handle_minute_tick(ctx, NULL);
+	
+	window_init(&window_root, "Countdown");
+	window_set_fullscreen(&window_root, true);
+	window_set_background_color(&window_root, GColorBlack);
+	window_stack_push(&window_root, true);
+	window_set_click_config_provider(&window_root, (ClickConfigProvider) root_window_click_provider);
+	window_set_window_handlers(&window_root, (WindowHandlers){
+		.load = window_load
+	});
 }
 
 void pbl_main(void *params) {
