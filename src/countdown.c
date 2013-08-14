d1b62b067394da60d23032b3a6d8e5fafb0f415b
Matthew Congrove <mattcongrove@gmail.com>
2013-08-14 11:22:46 -0400

    Custom-defined label options

--- src/countdown.c
+++ src/countdown.c
@@ -31,8 +31,7 @@
 #define APP_UUID { 0x35, 0xBA, 0x1E, 0x8E, 0x25, 0x65, 0x4C, 0x78, 0x95, 0xF6, 0xF9, 0x33, 0x53, 0x30, 0x8B, 0x04 }
 #define APP_TYPE APP_INFO_STANDARD_APP
 #define NUM_MENU_SECTIONS 1
-#define NUM_MENU_ITEMS 5
-#define LABEL_ROW "Days Remaining"
+#define NUM_MENU_ITEMS 6
 
 PBL_APP_INFO(APP_UUID, "Countdown", "Matthew Congrove", 1, 0, RESOURCE_ID_IMAGE_MENU_ICON, APP_TYPE);
 
@@ -40,13 +39,14 @@ Window window_root;
 Window window_menu;
 TextLayer label_time;
 TextLayer label_countdown;
-TextLayer label_row;
+TextLayer label_text_one;
 SimpleMenuLayer menu_layer;
 SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
 SimpleMenuItem menu_items[NUM_MENU_ITEMS];
 
 char timeText[] = "00:00";
 char countText[4];
+char* labelText;
 char settingTextMonth[10];
 char settingTextDay[10];
 char settingTextYear[10];
@@ -59,6 +59,7 @@ static int EVENT_YEAR = 2013;
 static int EVENT_HOUR = 12;
 static int EVENT_MINUTE = 0;
 static int EVENT_SECOND = 0;
+static int EVENT_LABEL = 0;
 
 bool calculate_countdown() {
 	PblTm now;
@@ -99,8 +100,9 @@ bool calculate_countdown() {
 	}
 	
 	string_format_time(timeText, sizeof(timeText), time_format, &now);
-	
 	text_layer_set_text(&label_time, timeText);
+	
+	text_layer_set_text(&label_text_two, labelText);
 	
 	return true;
 }
@@ -112,77 +114,128 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
 	calculate_countdown();
 }
 
-char* menu_set_month(int value, int index) {
+char* menu_set_month(int index) {
 	if(EVENT_MONTH > 12) { EVENT_MONTH = 1; }
 	
 	snprintf(settingTextMonth, 100, "%d", EVENT_MONTH);
 	
-	menu_items[index].subtitle = settingTextMonth;
-	layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	if(index) {
+		menu_items[index].subtitle = settingTextMonth;
+		layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	}
 	
 	return settingTextMonth;
 }
 
-char* menu_set_day(int value, int index) {
+char* menu_set_day(int index) {
 	if(EVENT_DAY > 31) { EVENT_DAY = 1; }
 	
 	snprintf(settingTextDay, 100, "%d", EVENT_DAY);
 	
-	menu_items[index].subtitle = settingTextDay;
-	layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	if(index) {
+		menu_items[index].subtitle = settingTextDay;
+		layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	}
 	
 	return settingTextDay;
 }
 
-char* menu_set_year(int value, int index) {
+char* menu_set_year(int index) {
 	if(EVENT_YEAR > 2020) { EVENT_YEAR = 2013; }
 	
 	snprintf(settingTextYear, 100, "%d", EVENT_YEAR);
 	
-	menu_items[index].subtitle = settingTextYear;
-	layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	if(index) {
+		menu_items[index].subtitle = settingTextYear;
+		layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	}
 	
 	return settingTextYear;
 }
 
-char* menu_set_hour(int value, int index) {
+char* menu_set_hour(int index) {
 	if(EVENT_HOUR > 23) { EVENT_HOUR = 0; }
 	
 	snprintf(settingTextHour, 100, "%d", EVENT_HOUR);
 	
-	menu_items[index].subtitle = settingTextHour;
-	layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	if(index) {
+		menu_items[index].subtitle = settingTextHour;
+		layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	}
 	
 	return settingTextHour;
 }
 
-char* menu_set_minute(int value, int index) {
+char* menu_set_minute(int index) {
 	if(EVENT_MINUTE > 59) { EVENT_MINUTE = 0; }
 	
 	snprintf(settingTextMinute, 100, "%d", EVENT_MINUTE);
 	
-	menu_items[index].subtitle = settingTextMinute;
-	layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	if(index) {
+		menu_items[index].subtitle = settingTextMinute;
+		layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	}
 	
 	return settingTextMinute;
+}
+
+char* menu_set_text(int index) {
+	if(EVENT_LABEL > 5) { EVENT_LABEL = 0; }
+	
+	switch(EVENT_LABEL) {
+		case 0:
+			labelText = "the Wedding";
+			break;
+		case 1:
+			labelText = "Baby Arrives";
+			break;
+		case 2:
+			labelText = "My Birthday";
+			break;
+		case 3:
+			labelText = "Our Anniversary";
+			break;
+		case 4:
+			labelText = "Christmas";
+			break;
+		case 5:
+			labelText = "the New Year";
+			break;
+	}
+	
+	if(index) {
+		menu_items[index].subtitle = labelText;
+		layer_mark_dirty(simple_menu_layer_get_layer(&menu_layer));
+	}
+	
+	return labelText;
 }
 
 void menu_select_callback(int index, void *ctx) {
 	switch(index) {
 		case 0:
-			menu_set_month(EVENT_MONTH++, index);
+			EVENT_MONTH++;
+			menu_set_month(index);
 			break;
 		case 1:
-			menu_set_day(EVENT_DAY++, index);
+			EVENT_DAY++;
+			menu_set_day(index);
 			break;
 		case 2:
-			menu_set_year(EVENT_YEAR++, index);
+			EVENT_YEAR++;
+			menu_set_year(index);
 			break;
 		case 3:
-			menu_set_hour(EVENT_HOUR++, index);
+			EVENT_HOUR++;
+			menu_set_hour(index);
 			break;
 		case 4:
-			menu_set_minute(EVENT_MINUTE++, index);
+			EVENT_MINUTE++;
+			menu_set_minute(index);
+			break;
+		case 5:
+			EVENT_LABEL++;
+			menu_set_text(index);
 			break;
 	}
 	
@@ -207,31 +260,37 @@ void menu_open() {
 	
 	menu_items[0] = (SimpleMenuItem){
 		.title = "Month",
-		.subtitle = menu_set_month(EVENT_MONTH, 0),
+		.subtitle = menu_set_month(0),
 		.callback = menu_select_callback
 	};
 	
 	menu_items[1] = (SimpleMenuItem){
 		.title = "Day",
-		.subtitle = menu_set_day(EVENT_DAY, 1),
+		.subtitle = menu_set_day(1),
 		.callback = menu_select_callback
 	};
 	
 	menu_items[2] = (SimpleMenuItem){
 		.title = "Year",
-		.subtitle = menu_set_year(EVENT_YEAR, 2),
+		.subtitle = menu_set_year(2),
 		.callback = menu_select_callback
 	};
 	
 	menu_items[3] = (SimpleMenuItem){
 		.title = "Hour",
-		.subtitle = menu_set_hour(EVENT_HOUR, 3),
+		.subtitle = menu_set_hour(3),
 		.callback = menu_select_callback
 	};
 	
 	menu_items[4] = (SimpleMenuItem){
 		.title = "Minute",
-		.subtitle = menu_set_minute(EVENT_MINUTE, 4),
+		.subtitle = menu_set_minute(4),
+		.callback = menu_select_callback
+	};
+	
+	menu_items[5] = (SimpleMenuItem){
+		.title = "Days Until...",
+		.subtitle = menu_set_text(5),
 		.callback = menu_select_callback
 	};
 	
@@ -272,14 +331,21 @@ void window_load(Window *window) {
 	text_layer_set_font(&label_countdown, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
 	layer_add_child(&window_root.layer, &label_countdown.layer);
 	
-	text_layer_init(&label_row, window_root.layer.frame);
-	text_layer_set_text_color(&label_row, GColorWhite);
-	text_layer_set_background_color(&label_row, GColorClear);
-	text_layer_set_text_alignment(&label_row, GTextAlignmentCenter);
-	layer_set_frame(&label_row.layer, GRect(0, 110, 144, 25));
-	text_layer_set_text(&label_row, LABEL_ROW);
-	text_layer_set_font(&label_row, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-	layer_add_child(&window_root.layer, &label_row.layer);
+	text_layer_init(&label_text_one, window_root.layer.frame);
+	text_layer_set_text_color(&label_text_one, GColorWhite);
+	text_layer_set_background_color(&label_text_one, GColorClear);
+	text_layer_set_text_alignment(&label_text_one, GTextAlignmentCenter);
+	layer_set_frame(&label_text_one.layer, GRect(0, 110, 144, 25));
+	text_layer_set_font(&label_text_one, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+	layer_add_child(&window_root.layer, &label_text_one.layer);
+	
+	text_layer_init(&label_text_two, window_root.layer.frame);
+	text_layer_set_text_color(&label_text_two, GColorWhite);
+	text_layer_set_background_color(&label_text_two, GColorClear);
+	text_layer_set_text_alignment(&label_text_two, GTextAlignmentCenter);
+	layer_set_frame(&label_text_two.layer, GRect(0, 110, 144, 25));
+	text_layer_set_font(&label_text_two, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+	layer_add_child(&window_root.layer, &label_text_two.layer);
 }
 
 void handle_init(AppContextRef ctx) {
